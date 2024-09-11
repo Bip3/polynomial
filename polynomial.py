f51960c6f65108212a34a951d13c552f4a365e35
Andrew King <kingandrew739@gmail.com>
2024-09-11 17:17:45 -0400

    Eval functions sir

--- polynomial.py
+++ polynomial.py
@@ -1,4 +1,6 @@
 class X:
+    def evaluate(self, i):
+        return Int(i)
     def __init__(self):
         pass
 
@@ -11,6 +13,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    def evaluate(self, i):
+        return self
+    
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +24,11 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, i):
+        p1 = self.p1.evaluate(i)
+        p2 = self.p2.evaluate(i)
+        return Int(p1.i + p2.i)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,7 +43,12 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, i):
+        p1 = self.p1.evaluate(i)
+        p2 = self.p2.evaluate(i)
+        return Int(p1.i * p2.i)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
